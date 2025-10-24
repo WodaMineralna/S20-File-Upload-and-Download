8df27c5a57e5426c8e7605dd83d9a41e38b37c8c
WodaMineralna <raknieborak65@gmail.com>
2025-10-24 18:02:44 +0200

    chore(multer): rename import in `app.js` for

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import path from "path";
 import express from "express";
 import cookieParser from "cookie-parser";
 
-import multer from "./config/multer.js";
+import multerConfig from "./config/multer.js";
 import sessionMiddleware from "./config/session.js";
 import csrfProtection from "./config/csrf.js";
 import flash from "./config/connect-flash.js";
@@ -19,7 +19,7 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.urlencoded({ extended: false }));
-app.use(multer);
+app.use(multerConfig);
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(cookieParser());
