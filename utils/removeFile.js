199cfa62cca426b24c48ba8ec60f65b17a57b117
WodaMineralna <raknieborak65@gmail.com>
2025-10-24 18:03:12 +0200

    refactor(removeFile): implement custom logging and error thrower into `removeFile.js`

--- utils/removeFile.js
+++ utils/removeFile.js
@@ -1,7 +1,12 @@
 import fs from "fs";
 
+import { newError, createLogger } from "./index.js";
+
+const log = createLogger(import.meta.url);
+
 export default function removeFile(filePath) {
   return fs.unlink(filePath, (err) => {
-    if (err) throw new Error(err);
+    log("error", err);
+    if (err) throw newError("An error occured while deleting a file", err);
   });
 }
