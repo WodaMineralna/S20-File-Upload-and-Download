48a0854fcba7487a7e725a69bb25c0a4e4887cc7
WodaMineralna <raknieborak65@gmail.com>
2025-10-25 14:21:35 +0200

    fix(utils): handle missing file edge case in `removeFile`
    - Previously when a file was already missing, `removeFile` threw an error instead of returning
    - Now, `removeFile` util silently ignores `ENOENT` errors when file does not exist

--- utils/removeFile.js
+++ utils/removeFile.js
@@ -6,7 +6,11 @@ const log = createLogger(import.meta.url);
 
 export default function removeFile(filePath) {
   return fs.unlink(filePath, (err) => {
-    log("error", err);
-    if (err) throw newError("An error occured while deleting a file", err);
+    if (err) {
+      log("error", err);
+
+      if (err.code === "ENOENT") return;
+      throw newError("An error occured while deleting a file", err);
+    }
   });
 }
